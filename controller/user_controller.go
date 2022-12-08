b42b7cad16e46b1a910382fba01e80936eb0ed55
DionFauzi <fauzidion46@gmail.com>
2022-12-08 15:33:35 +0700

    add user

---
+++ controller/user_controller.go
@@ -0,0 +1,213 @@
+package controller
+
+import (
+	"TokoBelanja/helper"
+	"TokoBelanja/model/input"
+	"TokoBelanja/model/response"
+	"TokoBelanja/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userController struct {
+	userService service.UserService
+}
+
+func NewUserController(userService service.UserService) *userController {
+	return &userController{userService}
+}
+
+func (h *userController) RegisterUser(c *gin.Context) {
+	var input input.UserRegisterInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	userData, err := h.userService.RegisterUser(input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	userResponse := response.UserRegisterResponse{
+		ID:        userData.ID,
+		FullName:  userData.FullName,
+		Email:     userData.Email,
+		Password:  userData.Password,
+		Balance:   userData.Balance,
+		CreatedAt: userData.CreatedAt,
+	}
+
+	c.JSON(
+		http.StatusCreated,
+		helper.NewResponse(
+			http.StatusCreated,
+			"created",
+			userResponse,
+		),
+	)
+}
+
+func (h *userController) LoginUser(c *gin.Context) {
+	var input input.UserLoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	token, err := h.userService.LoginUser(input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	userResponse := response.UserLoginResponse{
+		Token: token,
+	}
+
+	c.JSON(
+		http.StatusOK,
+		helper.NewResponse(
+			http.StatusOK,
+			"ok",
+			userResponse,
+		),
+	)
+}
+
+func (h *userController) PatchTopUpUser(c *gin.Context) {
+	var input input.UserPatchTopUpInput
+
+	id_user := c.MustGet("currentUser").(int)
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	userData, err := h.userService.TopUpUser(id_user, input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	message := "Your balance has been succcessfully updated to Rp " + strconv.Itoa(userData.Balance)
+	userResponse := response.UserPatchTopUpResponse{
+		Message: message,
+	}
+
+	c.JSON(
+		http.StatusOK,
+		helper.NewResponse(
+			http.StatusOK,
+			"ok",
+			userResponse,
+		),
+	)
+}
+
+func (h *userController) RegisterAdmin(c *gin.Context) {
+	var input input.UserRegisterInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	userData, err := h.userService.RegisterAdmin(input)
+	if err != nil {
+		errors := helper.GetErrorData(err)
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			helper.NewErrorResponse(
+				http.StatusUnprocessableEntity,
+				"failed",
+				errors,
+			),
+		)
+		return
+	}
+
+	userResponse := response.UserRegisterResponse{
+		ID:        userData.ID,
+		FullName:  userData.FullName,
+		Email:     userData.Email,
+		Password:  userData.Password,
+		Balance:   userData.Balance,
+		CreatedAt: userData.CreatedAt,
+	}
+
+	c.JSON(
+		http.StatusCreated,
+		helper.NewResponse(
+			http.StatusCreated,
+			"created",
+			userResponse,
+		),
+	)
+}
---
+++ service/user_service.go
@@ -0,0 +1,97 @@
+package service
+
+import (
+	"TokoBelanja/middleware"
+	"TokoBelanja/model/entity"
+	"TokoBelanja/model/input"
+	"TokoBelanja/repository"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService interface {
+	RegisterUser(input input.UserRegisterInput) (entity.User, error)
+	RegisterAdmin(input input.UserRegisterInput) (entity.User, error)
+	LoginUser(userInput input.UserLoginInput) (string, error)
+	TopUpUser(id_user int, input input.UserPatchTopUpInput) (entity.User, error)
+}
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserService(userRepository repository.UserRepository) *userService {
+	return &userService{userRepository}
+}
+
+func (s *userService) RegisterUser(input input.UserRegisterInput) (entity.User, error) {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.MinCost)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	user := entity.User{
+		FullName: input.FullName,
+		Email:    input.Email,
+		Password: string(passwordHash),
+		Role:     "customer",
+		Balance:  0,
+	}
+
+	return s.userRepository.Save(user)
+}
+
+func (s *userService) RegisterAdmin(input input.UserRegisterInput) (entity.User, error) {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.MinCost)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	user := entity.User{
+		FullName: input.FullName,
+		Email:    input.Email,
+		Password: string(passwordHash),
+		Role:     "admin",
+		Balance:  0,
+	}
+
+	return s.userRepository.Save(user)
+}
+
+func (s *userService) LoginUser(userInput input.UserLoginInput) (string, error) {
+	userData, err := s.userRepository.FindByEmail(userInput.Email)
+	if err != nil {
+		return "", err
+	}
+	if userData.ID == 0 {
+		return "", errors.New("user not found")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(userData.Password), []byte(userInput.Password))
+	if err != nil {
+		return "", errors.New("wrong password")
+	}
+
+	return middleware.GenerateToken(userData.ID, userData.Role)
+}
+
+func (s *userService) TopUpUser(id_user int, input input.UserPatchTopUpInput) (entity.User, error) {
+	userData, err := s.userRepository.FindById(id_user)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	balance := userData.Balance + input.Balance
+
+	user := entity.User{
+		Balance: balance,
+	}
+
+	_, err = s.userRepository.Update(id_user, user)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return s.userRepository.FindById(id_user)
+}
