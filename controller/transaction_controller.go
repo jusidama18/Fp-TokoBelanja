bce0d699ba237ef26c6fe61c2917593ffb76a523
jusidama18 <jusidama18@gmail.com>
2023-02-19 09:25:43 +0700

    Update Swagger Transactions

--- controller/transaction_controller.go
+++ controller/transaction_controller.go
@@ -19,6 +19,14 @@ func NewTransactionController(transService service.TransactionService) *transact
 	}
 }
 
+// @Summary Create Transaction
+// @Description Create Transaction by Data Provided
+// @Tags Transactions
+// @Accept json
+// @Produce json
+// @Param data body input.CreateTransactionRequest true "Create Transaction"
+// @Success 200 {object} helper.Response{data=response.TransactionBillResponse}
+// @Router /transactions [post]
 func (t *transactionController) CreateTransaction(c *gin.Context) {
 	var req input.CreateTransactionRequest
 
@@ -26,62 +34,63 @@ func (t *transactionController) CreateTransaction(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&req)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, helper.NewErrorResponse(http.StatusUnprocessableEntity, "BAD_REQUEST", err.Error()))
+		helper.Error(c, http.StatusUnprocessableEntity, "BAD_REQUEST", err.Error())
 		return
 	}
 
 	err = c.ShouldBindUri(&req)
 	if err != nil {
 		errors := helper.GetErrorData(err)
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, helper.NewErrorResponse(http.StatusBadRequest, "BAD_REQUEST", errors))
+		helper.Error(c, http.StatusBadRequest, "BAD_REQUEST", errors)
 		return
 	}
 
 	resp, err := t.transService.CreateTransaction(userID, req)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, helper.NewErrorResponse(http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", err.Error()))
+		helper.Error(c, http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", err.Error())
 		return
 	}
-
-	c.JSON(
-		http.StatusCreated,
-		helper.NewResponse(http.StatusCreated, "You have successfully purchased the product", resp),
-	)
+	helper.Success(c, http.StatusCreated, "You have successfully purchased the product", resp)
 }
 
+// @Summary Get My Transaction
+// @Description Get My Transaction
+// @Tags Transactions
+// @Accept json
+// @Produce json
+// @Success 200 {object} helper.Response{data=[]response.MyTransactionResponse}
+// @Router /transactions/my-transactions [get]
 func (t *transactionController) FindMyTransactions(c *gin.Context) {
 	userID := c.MustGet("currentUser").(int)
 	resp, err := t.transService.FindMyTransaction(userID)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, helper.NewErrorResponse(http.StatusInternalServerError, "UNAUTHORIZED", err.Error()))
+		helper.Error(c, http.StatusUnauthorized, "UNAUTHORIZED", err.Error())
 		return
 	}
-
-	c.JSON(
-		http.StatusOK,
-		helper.NewResponse(http.StatusCreated, "successfully get your transaction history", resp),
-	)
+	helper.Success(c, http.StatusCreated, "successfully get your transaction history", resp)
 }
 
+// @Summary Get All User Transaction
+// @Description Get All User Transaction
+// @Tags Transactions
+// @Accept json
+// @Produce json
+// @Success 200 {object} helper.Response{data=[]response.UserTransactionResponse}
+// @Router /transactions/user-transactions [get]
 func (t *transactionController) FindUserTransaction(c *gin.Context) {
 	userID := c.MustGet("currentUser").(int)
 	userRole := c.MustGet("roleUser").(string)
 
 	if userRole != "admin" {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, helper.NewErrorResponse(http.StatusInternalServerError, "UNAUTHORIZED", gin.H{
-			"error": "user not authorized",
-		}))
+		helper.Error(c, http.StatusUnauthorized, "UNAUTHORIZED", gin.H{"error": "user not authorized"})
 		return
 	}
 
 	resp, err := t.transService.FindUserTransaction(userID)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, helper.NewErrorResponse(http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", err.Error()))
+		helper.Error(c, http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", err.Error())
 		return
 	}
 
-	c.JSON(
-		http.StatusOK,
-		helper.NewResponse(http.StatusCreated, "successfully get all transaction history", resp),
-	)
+	helper.Success(c, http.StatusCreated, "successfully get all transaction history", resp)
 }
